23bf2572468ff9b27eaeb13e364ec347615630b1
mintleaves <abduaziz789999@gmail.com>
2024-07-15 19:18:24 +0600

    hero section complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,45 @@
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+import {
+  useGLTF,
+  Stage,
+  PresentationControls,
+  Environment,
+} from "@react-three/drei";
 import "./hero.scss";
 import { motion } from "framer-motion";
 
 function Model(props) {
   const { scene } = useGLTF("/mechanical_spider.glb");
-  return <primitive object={scene} {...props} />
+  return <primitive object={scene} {...props} />;
 }
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="text_container">
-        <h2>MINT LEAVES</h2>
-        <h1>Crafting animated visuals, UI design and Full-stack web apps</h1>
-        <div className="button_scroll_wrapper">
-          <div className="buttons">
-            <button>My Works</button>
-            <button>Contact Me</button>
-          </div>
-          <div className="scroll_wrapper">
+      <motion.div className="text_container" variants={textVariants} initial="initial" animate="animate">
+        <motion.h2 variants={textVariants}>MINT LEAVES</motion.h2>
+        <motion.h1 variants={textVariants}>Crafting animated visuals, UI design and Full-stack web apps</motion.h1>
+        <motion.div variants={textVariants} className="button_scroll_wrapper">
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>My Works</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.div variants={textVariants} className="scroll_wrapper">
             <div className="scroll_animation">
               <a href="#About">
                 <div className="scroll_icon">
@@ -37,19 +57,25 @@ const Hero = () => {
                 </div>
               </a>
             </div>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
 
       <div className="threed_container">
-        <Canvas dpr={[1,2]} shadows camera={{ fov: 45}} >
-          <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+        <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+          <PresentationControls
+            speed={1.5}
+            global
+            zoom={0.5}
+            polar={[-0.1, Math.PI / 4]}
+          >
             <Stage environment={null}>
               <Model scale={0.01} />
             </Stage>
           </PresentationControls>
         </Canvas>
       </div>
+
     </div>
   );
 };
